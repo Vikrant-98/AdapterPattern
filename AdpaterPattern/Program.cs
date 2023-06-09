72577e1d320f51a7479ecd367ad784bdaafc1e96
Vikrant Chitte <vikrant@easyrewardz.com>
2023-06-09 12:20:27 +0530

    Added Changes

---
+++ AdpaterPattern/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+interface Bird
+{
+    void fly();
+    void makeSound();
+}
+
+class Sparrow : Bird
+{
+    public void fly()
+    {
+        Console.WriteLine("Flying");
+    }
+    public void makeSound()
+    {
+        Console.WriteLine("Chirp Chirp");
+    }
+}
+
+interface ToyDuck
+{
+    void squeak();
+}
+
+class PlasticToyDuck : ToyDuck
+{
+    public void squeak()
+    {
+        Console.WriteLine("Squeak");
+    }
+}
+
+class BirdAdapter : ToyDuck
+{
+    Bird bird;
+    public BirdAdapter(Bird bird)
+    {
+        this.bird = bird;
+    }
+
+    public void squeak()
+    {
+        bird.makeSound();
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Sparrow sparrow = new Sparrow();
+        ToyDuck toyDuck = new PlasticToyDuck();
+
+        ToyDuck birdAdapter = new BirdAdapter(sparrow);
+
+        Console.WriteLine("Sparrow...");
+        sparrow.fly();
+        sparrow.makeSound();
+
+        Console.WriteLine("ToyDuck...");
+        toyDuck.squeak();
+
+        Console.WriteLine("BirdAdapter...");
+        birdAdapter.squeak();
+    }
+}
